bcd9adfb56f3ae1f3c31266c8b322d2241d191f1
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-11 20:04:49 -0500

    Variables

---
+++ js/variabes.js
@@ -0,0 +1,20 @@
+//Variables
+
+var producto = 'Audifonos Gamer';//Iniciar y asignar valor a variable
+var disponible;
+
+// producto = true; //Reasignamos variable 
+disponible = true;
+
+var producto1 = 'computadora',
+    disponible1 = true,
+    categria = 'computadoras';//Inicializacion y asignacion de valores a varias variables
+
+var _disponible;//no se permite usar numeros al incicio ni guion  medio
+
+//Estilos para lkas variables
+var nombre_producto = 'monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //camelcase
+var NombreProducto = 'Monitor HD'; //Pascal Case usado normalmente para la creacion de clases
+
+console.log(producto);
