02a497304cb62fd205fe34a2df5d329589d57185
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-11 20:27:37 -0500

    string

---
+++ js/strings.js
@@ -0,0 +1,12 @@
+//String o cadenas de texto
+
+const producto0 = "Monitor de 10\"";
+const producto = "Monitor de 20 Pulgadas";//mas usada
+const producto2 = String('Monitor de 30 pulgadas');
+const producto3 = new String('Monitor de 50 pulgadas');
+
+
+console.log(producto0);
+console.log(producto);
+console.log(typeof producto2);//typeof para saber el tipo de variable o constante
+console.log(producto3);
