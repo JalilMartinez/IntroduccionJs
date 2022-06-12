57420b7006ccead53b94fa99978d0b326d0834c4
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-11 20:18:15 -0500

    let y const

---
+++ js/variablesLet.js
@@ -0,0 +1,18 @@
+//Variables con Let
+
+let producto = 'Audifonos Gamer';//Iniciar y asignar valor a variable
+let disponible;
+
+producto = true; //Reasignamos variable 
+disponible = true;
+
+let producto1 = 'computadora',
+    disponible1 = true,
+    categria = 'computadoras';//Inicializacion y asignacion de valores a varias variables
+
+let _disponible;//no se permite usar numeros al incicio ni guion  medio
+
+//Estilos para las variables
+let nombre_producto = 'monitor HD'; //underscore
+let nombreProducto = 'Monitor HD'; //camelcase
+let NombreProducto = 'Monitor HD'; //Pascal Case usado normalmente para la creacion de clases
