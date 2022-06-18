d4ad1c3b537bf36b293bb8cbe9c9a3068dbe9b7c
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-18 17:24:58 -0500

    metodos string

--- js/strings.js
+++ js/strings.js
@@ -1,5 +1,6 @@
 //String o cadenas de texto
 
+const tweet = 'Aprendiendo JavaScript con el curso de Desarrollo Web Completo'
 const producto0 = "Monitor de 10\"";
 const producto = "Monitor de 20 Pulgadas";//mas usada
 const producto2 = String('Monitor de 30 pulgadas');
@@ -10,3 +11,15 @@ console.log(producto0);
 console.log(producto);
 console.log(typeof producto2);//typeof para saber el tipo de variable o constante
 console.log(producto3);
+
+//Metodos
+console.log(producto.length);
+//IndexOf(para buscar algo en el string)
+console.log(tweet.indexOf('JavaScript'));
+console.log(producto2.indexOf('JavaScript'));/* -1 es porque no pudo encontrarlo*/
+
+
+//Includes (retorna true o false)
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('JavaScript'));/* -1 es porque no pudo encontrarlo*/
+
