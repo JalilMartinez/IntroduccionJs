dd2efc98926935ce05a9c3acaa81a3a139787160
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-18 17:46:18 -0500

    funcion math

---
+++ js/math.js
@@ -0,0 +1,17 @@
+//OIbjeto Math
+
+var resultado = 0;
+
+// resultado = Math.random();
+//  resultado = Math.PI;
+// resultado = Math.round(2.53333);
+// resultado = Math.ceil(2.53333);/*redondea hacia arriba*/
+// resultado = Math.floor(2.53333);/*redondea hacia abajo*/
+// resultado = Math.sqrt(2.53333);/*redondea hacia abajo*/
+// resultado = Math.abs(2.53333);/*redondea hacia abajo*/
+// resultado = Math.min(2, 5, 6, 8, 3);/*redondea hacia abajo*/
+// resultado = Math.max(2, 5, 6, 8, 3);/*redondea hacia abajo*/
+resultado = Math.ceil( Math.random() * 20);/*redondea hacia abajo*/
+
+
+console.log(resultado);
