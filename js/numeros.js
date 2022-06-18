ea9ce995ecdd7a643944606755d34856d0e17d4f
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-18 17:35:50 -0500

    numeros js

---
+++ js/numeros.js
@@ -0,0 +1,16 @@
+//Numeros
+
+
+const nombre = "Mazatl";
+const numero1 = 150;
+const numero2 = 200;
+
+console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+console.log(numero2 % numero1);/* se divide num2 entre num1 y el residuo es el resultado, se llama modulo*/
+
