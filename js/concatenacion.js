5b06a8ee38d8b3de2b9915da5a44f46274dc3019
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-20 10:18:22 -0500

    concatenacion strings

---
+++ js/concatenacion.js
@@ -0,0 +1,8 @@
+const nombre ='Jalil';
+const email = 'correo@correo.com';
+
+//Concatenacion
+console.log("nombre de cliente: " + nombre + " email: " + email);
+
+//Template strings - Strings literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`  );
