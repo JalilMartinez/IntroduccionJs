6a1fbd11c68f0a0d99fa8551bacbb6bc31ca5379
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-20 10:46:49 -0500

    destructuring objeto

---
+++ js/destructuring_ob.js
@@ -0,0 +1,26 @@
+
+const producto = {
+    nombreProduccto: "Monitor 30 pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+//Forma anterior
+const precioProducto = producto.precio;
+const nombreProducto = producto.nombreProduccto;
+
+// console.log(precioProducto);
+// console.log(nombreProducto);
+
+//Destructuring
+
+// const {precio} = producto;
+// const {nombreProduccto} = nombreProducto;
+
+// console.log(precio);
+// console.log(nombreProducto);
+
+//se puede colocar los que necesites en una sola linea 
+const {precio, nombreProduccto} = producto;
+console.log(precio);
+console.log(nombreProducto);
