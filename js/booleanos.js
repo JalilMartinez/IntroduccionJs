9969c06e9ddbce916bcf66985daac718b54b98b8
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-20 10:25:30 -0500

    booleans

---
+++ js/booleanos.js
@@ -0,0 +1,14 @@
+//Booleans
+//solo tienen dos estados "true" o "false"
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";/*ya no es boolean, es string*/
+
+console.log(boolean1);
+console.log(boolean2);
+console.log(boolean3);
+
+const boolean4 = new Boolean(true);/*Con constructor*/
+
+console.log(boolean4);/*este va a ser un objeto en realidad*/
+console.log(typeof boolean4);/*este va a ser un objeto en realidad*/
