a928a702fa445af2df8271f24ec5ec50382d0d9a
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-28 14:03:27 -0500

    a

--- js/funciones.js
+++ js/funciones.js
@@ -15,6 +15,15 @@ sumar2();
 //la diferencia entre la declaracion y la expresion es que, en la primera aunque se mande
 //a llamar antes funciona, en la expresion no sirve y se debe llamar despues de la funcion
 
+/*
+    Esto pasa porque javaScript se ejecuta en 2 vueltas (a esto se le llama hosting)
+      - La primer vuelta se le llama de creacion y la segunda de ejecución   
+      - En la primera vuelta se registran las funciones y en la segunda vuelta se registra lo demas
+      - La segunda no funciona asi porque no esta declarada con una sintaxis de function
+  
+*/ 
+
+
 //IIFE  se manda a llamar sola
 //previene que no se puedan leer las variables desde otros archivos
 (function() {
