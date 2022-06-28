bff71c697293e679293959266dec80a39b84b52c
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-28 14:08:25 -0500

    dif metodo vs funcion

---
+++ js/Met_vs_Fun.js
@@ -0,0 +1,5 @@
+const numero1 = 20;
+const numero2 = "20";
+
+console.log( parseInt(numero2));//parseint es una funcion (consta de una funcion a la cual se le mandan variables o solo se llaman)
+console.log( numero1.toString());// metodo consta de una variable llamando algo mas
