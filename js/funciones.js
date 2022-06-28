92f9bcc469fd90cc621428654b2a9953bc565589
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-06-28 13:55:35 -0500

    a

--- js/funciones.js
+++ js/funciones.js
@@ -1,19 +1,21 @@
 //Declaracion de Función
-
+sumar();
 function sumar(){
     console.log(10 * 10);
 }
 
-sumar();
+
 
 //Expresion de la función 
-
 const sumar2 = function(){
     console.log(3 + 3);
 }
 sumar2();
 
-//IIFE
+//la diferencia entre la declaracion y la expresion es que, en la primera aunque se mande
+//a llamar antes funciona, en la expresion no sirve y se debe llamar despues de la funcion
+
+//IIFE  se manda a llamar sola
 //previene que no se puedan leer las variables desde otros archivos
 (function() {
     console.log('Esto es una función')
