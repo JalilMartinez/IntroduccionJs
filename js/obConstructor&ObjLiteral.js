2fb5dec8ba532a99199047c7d2b99d3a355b9e70
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-07-02 18:27:09 -0500

    prototypes

--- js/obConstructor&ObjLiteral.js
+++ js/obConstructor&ObjLiteral.js
@@ -14,7 +14,22 @@ function Producto(nombre, precio) {
     this.nombre=nombre;
     this.precio=precio;
 }
+//prototypes permite usar funciones que solo se utilizan en un objeto en especifico
+Producto.prototype.formatearProducto= function(){
+    return `${this.nombre} - ${this.precio}`;
+}
 
 const producto2 = new Producto('Monitor curvo',800);
 
 console.log(producto2)
+
+
+
+/*function formatearProducto(producto){
+    return `${producto.nombre} - ${producto.precio}`;
+}
+console.log(formatearProducto(producto2));*/
+
+
+
+console.log(producto2.formatearProducto());
