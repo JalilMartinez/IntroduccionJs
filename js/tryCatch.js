334b3ea0b07da21249e41b25fe409aa7f40f36f7
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-07-04 10:11:36 -0500

    try Catch

---
+++ js/tryCatch.js
@@ -0,0 +1,14 @@
+//try catch para poner ciertas lineas importantes del codigo y en caso de error, el codigo no deje de funcionar
+
+const num1 = 20;
+const num2 = 30;
+
+console.log(num1);
+
+try {
+    console.log(num3);
+} catch (error) {
+    console.log(error)
+}
+
+console.log(num2);
